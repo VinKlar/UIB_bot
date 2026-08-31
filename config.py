6cb52043614db5ed884fd856be3f2912d4c59ae8
VinKlar <mailforchat58@gmail.com>
2026-08-31 15:39:21 +0700

    asdasda

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 TOKEN = os.getenv("MAX_TOKEN")
-API_URL = "https://platform-api2.max.ru"
+API_URL = "https://platform-api.max.ru"
 
 if not TOKEN:
     raise ValueError("MAX_TOKEN не найден")
